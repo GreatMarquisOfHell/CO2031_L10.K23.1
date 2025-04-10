901169436c90465e362a5871dfebfdcbce06a673
GreatMarquisOfHell <trankienquoc02@gmail.com>
2025-04-10 15:51:23 +0700

    Create loss_funcs.py

---
+++ ann/layer/loss_funcs.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def mse(y_true, y_pred):
+    return np.mean((y_true - y_pred) ** 2)
+
+
+def mse_derivative(y_true, y_pred):
+    return 2 * (y_pred - y_true) / y_true.size
+
+
+def CCE(y_true, y_pred):
+    y_pred = np.clip(y_pred, 1e-12, 1 - 1e-12)
+    return -np.sum(y_true * np.log(y_pred))
+
+
+def CCE_derivative(y_true, y_pred):
+    return -np.array(y_true) / y_pred
